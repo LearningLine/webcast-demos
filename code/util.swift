ce4ca76cf41d5870ebf1fc1c08127db39902beb9
needham-develop <needham@develop.com>
2015-05-06 12:13:54 -0700

    Trim newline

--- code/util.swift
+++ code/util.swift
@@ -4,5 +4,5 @@ func promptForInput(prompt: String) -> String {
     print(prompt)
     var stdin = NSFileHandle.fileHandleWithStandardInput()
     var data = stdin.availableData
-    return NSString(data: data, encoding: NSUTF8StringEncoding)! as String
+    return (NSString(data: data, encoding: NSUTF8StringEncoding)! as String).stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
 }
